425bd7e45d1734e05701afb1b728be57e574820b
HafeezSalim <59242442+HafeezSalim@users.noreply.github.com>
2025-07-13 12:05:39 +0800

    Complete getComputerChoice function

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("Hello World!");
+function getComputerChoice () {
+    let computerChoice = "";
+    let randomNumber = Math.floor(Math.random() * 3 + 1);
+    if (randomNumber == 1) {
+        computerChoice = "rock";
+    } else if (randomNumber == 2) {
+        computerChoice = "paper";
+    } else {
+        computerChoice = "scissors";
+    }
+    return computerChoice
+}
+
+console.log(getComputerChoice());
